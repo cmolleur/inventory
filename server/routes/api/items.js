6bf37fa72ee168e1704c2f83408222c9413f4983
Colette Molleur <colette.molleur@gmail.com>
2016-06-05 17:18:50 -0400

    y

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -1,7 +1,7 @@
 //modules!
 var express = require('express');
 var router = express.Router();
-var Item = require('../../models/item');
+var Item = require('./models/item');
 
 //routing!
 router.get('/', function(req, res){
